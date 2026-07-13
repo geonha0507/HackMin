5d96ca2cca939403006c46cee2a661f12724dedd
윤현식 <162391539+YHs1k@users.noreply.github.com>
2026-07-13 22:23:49 +0900

    인증(/auth) 및 마이페이지(/me) API

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,53 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from .models import Address
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'phone', 'nickname', 'role', 'status', 'date_joined']
+        read_only_fields = ['id', 'role', 'status', 'date_joined']
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=4)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'phone', 'nickname']
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError('이미 사용 중인 아이디입니다.')
+        return value
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(role=User.Role.CUSTOMER, **validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True, min_length=4)
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = [
+            'id', 'label', 'address', 'detail', 'postal_code',
+            'latitude', 'longitude', 'is_default', 'created_at',
+        ]
+        read_only_fields = ['id', 'created_at']
