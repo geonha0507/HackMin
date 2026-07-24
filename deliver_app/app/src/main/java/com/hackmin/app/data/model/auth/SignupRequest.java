72ed1800a7127cdf7768572324f66dd8bcea71dc
seungjuuun01 <ssj010220@dankook.ac.kr>
2026-07-24 09:47:52 +0900

    feat: 회원가입 주민등록번호 입력 추가 (만 14세 검증 + 뒷자리 마스킹)

--- deliver_app/app/src/main/java/com/hackmin/app/data/model/auth/SignupRequest.java
+++ deliver_app/app/src/main/java/com/hackmin/app/data/model/auth/SignupRequest.java
@@ -22,18 +22,22 @@ public class SignupRequest {
     @SerializedName("phone")
     private String phone;
 
+    @SerializedName("rrn")
+    private String rrn;
+
     @SerializedName("terms_agreed")
     private boolean termsAgreed;
 
     public SignupRequest(String username, String email, String password,
                          String nickname, String name, String phone,
-                         boolean termsAgreed) {
+                         String rrn, boolean termsAgreed) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.nickname = nickname;
         this.name = name;
         this.phone = phone;
+        this.rrn = rrn;
         this.termsAgreed = termsAgreed;
     }
 }
