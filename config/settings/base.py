8636016f5b470a4387df1789a56150e4a1168f2d
YHs1k <yhs304031@gmail.com>
2026-07-30 15:05:58 +0900

    fix: REVIEW_IMAGE_ROOT 설정 복원 (rebase 충돌 해결 중 누락)

--- config/settings/base.py
+++ config/settings/base.py
@@ -168,6 +168,10 @@ if os.environ.get('USE_S3', '0') == '1':
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+# --- 리뷰 이미지 전용 스토리지 (민감정보 분리) ------------------------------
+REVIEW_IMAGE_ROOT = os.environ.get('REVIEW_IMAGE_ROOT', str(MEDIA_ROOT))
+REVIEW_IMAGE_URL = '/media/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
